42fb8151c968b1c8fe27413e1f301e4f86eb4d3c
Dennis Hillmann <dh@udmedia.de>
2015-06-15 09:23:14 +0200

    Fix error when having tabs and spaces mixed as indentation

--- normalize_indentation.py
+++ normalize_indentation.py
@@ -25,17 +25,28 @@ class NormalizeIndentationCommand(sublime_plugin.TextCommand):
             print('converting spaces to tabs')
             self.view.run_command('unexpand_tabs')
             self.view.settings().set('tab_size', wanted_tabsize)
+            region = sublime.Region(0, self.view.size())
+            self.view.sel().add(region)
+            self.view.run_command('reindent')
+            self.view.sel().subtract(region)
         elif (view_translate_tabs_to_spaces == False and translate_tabs_to_spaces == True and view_tabsize == wanted_tabsize): #we have tabs but want spaces
             print('converting tabs to spaces')
+            region = sublime.Region(0, self.view.size())
+            self.view.sel().add(region)
+            self.view.run_command('reindent')
+            self.view.sel().subtract(region)
             self.view.run_command('expand_tabs')
             self.view.settings().set('translate_tabs_to_spaces', True)
         elif (view_translate_tabs_to_spaces == True and view_tabsize != wanted_tabsize): #we have spaces but the tab_size does not fit
-            time.sleep(1)
             print('setting right tabsize')
             self.view.run_command('unexpand_tabs') #first convert to tabs
             self.view.settings().set('tab_size', wanted_tabsize) # change size
             self.view.run_command('expand_tabs') #expand again
             self.view.settings().set('translate_tabs_to_spaces', True)
+            region = sublime.Region(0, self.view.size())
+            self.view.sel().add(region)
+            self.view.run_command('reindent')
+            self.view.sel().subtract(region)
         else:
             print('all is fine and dandy')
         print('Done normalizing indentation')
